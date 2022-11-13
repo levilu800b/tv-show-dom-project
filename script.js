fa2b6ad36241e17a965ad260ffde2bd11045affb
levilu800b <levilu800b@gmail.com>
2022-11-13 16:34:34 +0000

    js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //You can edit ALL of the code here
+const dropDown = document.getElementById("dropdown");
+const card = document.getElementById("cards");
+const allEpisodes = getAllEpisodes();
+const allShows = getAllShows();
+
+
+
+window.onload = setup;
